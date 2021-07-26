349a982bb9095a93d2db2e1d87e1d8b0598fdea9
gitMarco19 <marcoscanu2019@live.com>
2021-07-26 17:54:52 +0200

    Aggiunta di un commento per il file totalProcessingTime.m

--- Parte2/matlabCode/totalProcessingTime.m
+++ Parte2/matlabCode/totalProcessingTime.m
@@ -1,3 +1,6 @@
+%It is a function that computes the total processing time for a given set of jobs
+%It is used in the algorithmDP.m file
+
 function time = totalProcessingTime(jobs, p)
     temp = 0;
     for i = 1 : length(jobs)
