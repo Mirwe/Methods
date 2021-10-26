a97678cd182999d4a9268ce3d20cd1e5bdd987f5
Marco Scanu <marcoscanu2019@live.com>
2021-10-26 10:36:29 +0200

    Comments fixed

--- Parte3/Esercizio2/Es2.m
+++ Parte3/Esercizio2/Es2.m
@@ -8,9 +8,8 @@ quantita_acquistata = [500, 50, 1200, 13400, 400];
 scorta_finale = [20, 80, 800, 1450, 1100];
 
 %% Inventory KPI
-
 % Average stock 
-C = (scorta_iniziale + scorta_finale)/2;
+C = (scorta_iniziale + scorta_finale) / 2;
 
 % Total output flow
 Q = scorta_iniziale + quantita_acquistata - scorta_finale;
