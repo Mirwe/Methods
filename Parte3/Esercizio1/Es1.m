4e59c021e5f62e17659c25aafa7eb393ae19aba3
Marco Scanu <marcoscanu2019@live.com>
2021-10-26 10:36:13 +0200

    Comments fixed

--- Parte3/Esercizio1/Es1.m
+++ Parte3/Esercizio1/Es1.m
@@ -2,7 +2,7 @@ clear;
 clc;
 
 %% Initial data
-% Demand for a given product abut the last 8 months
+% Demand for a given product about the last 8 months
 months = (1 : 8)';
 demand = [580, 700, 775, 650, 585, 760, 790, 815]';
 
@@ -12,11 +12,11 @@ mdl = fitlm(months, demand, 'linear');
 
 test_demand = predict(mdl, months);
 figure
-plot(months,demand,'o',months,test_demand,'x')
-legend('Data','Linear Regression')
+plot(months, demand, 'o', months, test_demand, 'x')
+legend('Data', 'Linear Regression')
 
 %mean squared error
-mse_LR=immse(test_demand,demand);
+mse_LR = immse(test_demand, demand);
 
 %Prediction for the next months
 next_months = (1 : 16)';
@@ -24,21 +24,22 @@ LR_prediction = predict(mdl, next_months);
 
 
 %% Exponential Smoothing
+%exponential avarage constant
 alpha = 0.15;
-SE_prediction = zeros(1,9);
+SE_prediction = zeros(1, 9);
 
 %The prediction for the first demand is equal to the demand itself
 SE_prediction(1) = demand(1);
 
 % Predictions computation
-for i = 2 : 9
-    SE_prediction(i) = alpha * demand(i-1) + (1-alpha) * SE_prediction(i-1);
+for t = 1 : 8
+    SE_prediction(t + 1) = alpha * demand(t) + (1 - alpha) * SE_prediction(t);
 end
 
 %mean squared error
-mse_SE=immse(SE_prediction(1:8)',demand);
+mse_SE = immse(SE_prediction(1 : 8)', demand);
 
 figure
-plot(months,demand,'o',months,SE_prediction(1:8),'x')
-legend('Data','Exponential smoothing')
+plot(months, demand, 'o', months, SE_prediction(1 : 8), 'x')
+legend('Data', 'Exponential smoothing')
 
